6de865c5db8efe210e9c209cda547ef4a2622dae
codecraft <codecraftwt@gmail.com>
2025-09-30 16:09:13 +0530

    Refactor axios instance and enhance EventsList component to format upcoming holidays

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -5,14 +5,13 @@ const api = axios.create({
   baseURL: 'https://crm.walstartechnologies.com/api/',
   // baseURL: 'https://45c03307c9ff.ngrok-free.app/api/',
   headers: {
-    'Content-Type': 'application/json',  // 🔑 Important!
+    'Content-Type': 'application/json',  
   },
 });
 
 api.interceptors.request.use(
   async config => {
     const token = await AsyncStorage.getItem('token'); 
-    console.log('token---------------->', token);
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
